2316f564c21e01a4b9668674450c96475804ff80
Magno Gouveia <magno.gouveia@metasix.com.br>
2017-07-10 14:41:43 -0300

    changed project path

---
+++ clean/js/main.js
@@ -0,0 +1,136 @@
+/* important! for alignment, you should make things
+ * relative to the canvas' current width/height.
+ */
+let canvas = document.querySelector('#canvas')
+let ctx = canvas.getContext('2d')
+let date = new Date()
+let difference = 0
+let fps = 20
+let initialCactusPos = 800;
+let cactusPos = 400
+let jumping = false;
+let maxJumpHeight = 200;
+let actualPos = 0
+let cactusSpeed = 5;
+let podePula = true
+let relativePos = 0
+let gameOver = false
+let HighScore = localStorage.getItem('HighScore')
+
+document.addEventListener('keydown', function (e) {
+  if(e.code == 'Space'){
+    if(podePula && !gameOver) jumping = true;
+    if(gameOver){
+       jumping = false
+       actualPos = 0
+       gameOver = false;
+       cactusPos = initialCactusPos
+       if(localStorage.getItem('HighScore') < difference)
+        localStorage.setItem('HighScore',difference)
+      
+       HighScore = localStorage.getItem('HighScore')
+       date = new Date()
+       loop()
+    }
+  
+    podePula = false
+  }
+})
+
+function draw () {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  // ...drawing code...
+  let floor = canvas.height / 2 +20
+  let initialHeight = canvas.height / 2 +20
+
+  ctx.fillStyle = 'Grey'
+  ctx.fillRect(10, 10, canvas.width - 20, canvas.height - 20)
+  ctx.stroke()
+
+  ctx.lineWidth = 5
+  ctx.strokeStyle = 'yellow'
+  ctx.moveTo(30, canvas.height / 2 + 70)
+  ctx.lineTo(canvas.width - 30, canvas.height / 2 + 70)
+  ctx.stroke()
+
+  cactusGenerator(floor - 20,cactusPos)
+  cactusPos -= cactusSpeed
+
+  if(cactusPos < 0)
+    cactusPos = initialCactusPos;
+
+  ctx.lineWidth = 1
+  ctx.strokeStyle = 'black'
+  ctx.font = '30px Arial'
+  ctx.strokeText('Time: ' + difference, canvas.width / 2  - 200, 100)
+
+  ctx.strokeText('HighScore: ' + HighScore, canvas.width / 2, 100)
+
+  drawPersonagem(floor);
+
+  if(checkForCollision(50,cactusPos,70,relativePos)){
+    gameOver = true
+  }
+
+}
+
+function cactusGenerator (floor, cactusSpeed) {
+  ctx.strokeStyle = 'green'
+  ctx.lineWidth = 3
+  ctx.strokeRect(cactusPos, floor, 30, 70)
+}
+
+function checkForCollision(playerPos, objectPosW, AlturaObjeto, playerAltura) {
+  if(objectPosW <= playerPos){
+    if(playerAltura <= AlturaObjeto){
+      return true
+    }
+    return false
+  }
+  return false
+}
+
+function drawPersonagem(floor){
+  ctx.strokeStyle = 'red'
+  ctx.lineWidth = 5
+  if(actualPos == 0)
+    actualPos = floor
+  relativePos = (floor - actualPos );
+  // console.log(actualPos, relativePos)
+  // console.log(maxJumpHeight)
+  if(jumping && relativePos < maxJumpHeight)
+    ctx.strokeRect(50, actualPos-= 5, 50, 50)
+  else if( relativePos >= maxJumpHeight)
+    jumping = false
+
+  if( floor > actualPos && !jumping){
+    ctx.strokeRect(50, actualPos+=5, 50, 50)
+  }else if(!jumping){
+    ctx.strokeRect(50, floor, 50, 50)
+    actualPos = floor
+    podePula = true
+  }
+}
+
+
+function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function loop () {
+
+  while (!gameOver) {
+
+    let current = new Date()
+    difference = Math.floor((current - date ) / 1000)
+    //console.log(difference)
+
+    draw()
+
+    await sleep(fps)
+
+  }
+}
+
+loop()
